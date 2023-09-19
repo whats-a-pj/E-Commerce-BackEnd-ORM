eb583539973995b3c7043b8ad1c881463356dd58
Kaelyn P.J. Rasmussen <r4smussen.pj@gmail.com>
2023-09-18 21:01:08 -0600

    Added validations/constraints to product.js model

--- models/Product.js
+++ models/Product.js
@@ -13,22 +13,26 @@ Product.init(
       type: DataTypes.INTEGER,
       allowNull: false,
       primaryKey: true,
-      autoIncrement: true
+      autoIncrement: true,
     },
     product_name: {
       type: DataTypes.STRING,
-      allowNull: false
+      allowNull: false,
     },
     price: {
       type: DataTypes.DECIMAL,
       allowNull: false,
-      //todo validate that the value is a decimal
+      validate: {
+        isDecimal: true,
+      },
     },
     stock: {
       type: DataTypes.INTEGER,
       allowNull: false,
-      //todo set default value of 10
-      //todo validates that the value is numeric
+      min: 10,
+      validate: {
+        isNumeric: true,
+      },
     },
     category_id: {
       type: DataTypes.INTEGER,
